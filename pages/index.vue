e0a7cd96efd94ea164e5181a8b6be88dbd0dfe63
takashi-hash <d25hw41@gmail.com>
2021-08-30 00:54:23 +0900

    !

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page"><p class="text">test</P></div>
+  
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+  .page {
+    > .text {
+      color : red;
+    }
+  }
+</style>
